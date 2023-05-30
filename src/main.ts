d90119abec908b83e4863994a6f546bfa31f3e23
estarhp <112890331+estarhp@users.noreply.github.com>
2023-05-31 00:17:10 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+// @ts-ignore
+import App from "./App.vue"
 
 createApp(App).mount('#app').$nextTick(() => postMessage({ payload: 'removeLoading' }, '*'))
